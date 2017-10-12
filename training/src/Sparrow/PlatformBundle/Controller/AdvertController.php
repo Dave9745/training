49911860891521e3723eefeaa85f73ca8f3d322a
El Pirate <dtirnan@hotmail.com>
2017-10-13 01:16:07 +0200

    routing tests

--- training/src/Sparrow/PlatformBundle/Controller/AdvertController.php
+++ training/src/Sparrow/PlatformBundle/Controller/AdvertController.php
@@ -4,13 +4,22 @@ namespace Sparrow\PlatformBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+
 
 class AdvertController extends Controller
 {
     public function indexAction()
     {
-        $content = $this->get('templating')->render('SparrowPlatformBundle:Advert:index.html.twig', array('name' => 'Sparrow',
-                                                                                                           'firstname' => 'Dave'));
+      
+        $url = $this->get('router')->generate(
+            'sparrow_platform_view', 
+            array('id' => 5),
+            UrlGeneratorInterface::ABSOLUTE_URL
+        );
+        
+          $content = $this->get('templating')->render('SparrowPlatformBundle:Advert:index.html.twig', array('name' => 'Sparrow','firstname' => 'Dave', 'url' => $url));
+        
         return new Response($content);
     }
     
